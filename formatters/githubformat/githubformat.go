c1b096ab02e8f71dc2f428c6ced9efe44ebc670f
Blake Williams <blake@blakewilliams.me>
2024-12-19 14:07:09 -0500

    Add newline

--- formatters/githubformat/githubformat.go
+++ formatters/githubformat/githubformat.go
@@ -79,7 +79,7 @@ func (f *Formatter) Format(source string, i *manifest.Import, r manifest.Result)
 
 		var message strings.Builder
 
-		message.WriteString(fmt.Sprintf("<!-- %s -->", fingerprint))
+		message.WriteString(fmt.Sprintf("<!-- %s -->\n\n", fingerprint))
 
 		switch comment.Severity {
 		case manifest.SeverityError:
