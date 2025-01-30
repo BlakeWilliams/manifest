4803cf41c9c419ff3b91fc10b69377d2a7c3a9c9
Garvin Casimir <garvincasimir@users.noreply.github.com>
2025-01-30 13:07:54 -0500

    back to mocks

--- formatters/githubformat/githubformat_test.go
+++ formatters/githubformat/githubformat_test.go
@@ -1,61 +1,61 @@
 package githubformat
 
 import (
+	"fmt"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/blakewilliams/manifest"
 	"github.com/blakewilliams/manifest/github"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 type fakeGitHubClient struct {
-	comments          []github.Comment
-	reviewComments    []github.Comment
-	fileComments      []github.NewFileComment
-	resolvedComments  []github.Comment
-	unresolvedComments []github.Comment
+	mock.Mock
 }
 
 var _ GitHubClient = (*fakeGitHubClient)(nil)
 
 func (f *fakeGitHubClient) Comment(number int, comment string) error {
-	f.comments = append(f.comments, github.Comment{Body: comment})
-	return nil
+	args := f.Called(number, comment)
+	return args.Error(0)
 }
 
 func (f *fakeGitHubClient) FileComment(fc github.NewFileComment) error {
-	f.fileComments = append(f.fileComments, fc)
-	return nil
+	args := f.Called(fc)
+	return args.Error(0)
 }
 
 func (f *fakeGitHubClient) Comments(number int) ([]github.Comment, error) {
-	return f.comments, nil
+	args := f.Called(number)
+	return args.Get(0).([]github.Comment), args.Error(1)
 }
 
 func (f *fakeGitHubClient) ReviewComments(number int) ([]github.Comment, error) {
-	return f.reviewComments, nil
+	args := f.Called(number)
+	return args.Get(0).([]github.Comment), args.Error(1)
 }
 
 func (f *fakeGitHubClient) ResolveFileComment(comment github.Comment) error {
-	f.resolvedComments = append(f.resolvedComments, comment)
-	return nil
+	args := f.Called(comment)
+	return args.Error(0)
 }
 
 func (f *fakeGitHubClient) ResolveComment(comment github.Comment) error {
-	f.resolvedComments = append(f.resolvedComments, comment)
-	return nil
+	args := f.Called(comment)
+	return args.Error(0)
 }
 
 func (f *fakeGitHubClient) UnresolveFileComment(comment github.Comment) error {
-	f.unresolvedComments = append(f.unresolvedComments, comment)
-	return nil
+	args := f.Called(comment)
+	return args.Error(0)
 }
 
 func (f *fakeGitHubClient) UnresolveComment(comment github.Comment) error {
-	f.unresolvedComments = append(f.unresolvedComments, comment)
-	return nil
+	args := f.Called(comment)
+	return args.Error(0)
 }
 
 func TestFormat_FileComment(t *testing.T) {
@@ -82,17 +82,25 @@ func TestFormat_FileComment(t *testing.T) {
 	}
 
 	client := &fakeGitHubClient{}
+	client.On("FileComment", mock.MatchedBy(func(fc github.NewFileComment) bool {
+		return fc.Number == 1 &&
+			fc.File == "test.go" &&
+			fc.Line == 10 &&
+			fc.Side == "RIGHT" &&
+			strings.Contains(fc.Text, "Test comment") &&
+			strings.Contains(fc.Text, "> [!CAUTION]")
+	})).Return(nil)
+
+	client.On("Comment", 1, mock.MatchedBy(func(comment string) bool {
+		return strings.Contains(comment, "Test comment 2") &&
+			strings.Contains(comment, "> [!TIP]")
+	})).Return(nil)
+
 	formatter := New(io.Discard, client)
 	err := formatter.Format("test", i, result)
 	require.NoError(t, err)
 
-	require.Len(t, client.fileComments, 1)
-	require.Contains(t, client.fileComments[0].Text, "Test comment")
-	require.Contains(t, client.fileComments[0].Text, "> [!CAUTION]")
-
-	require.Len(t, client.comments, 1)
-	require.Contains(t, client.comments[0].Body, "Test comment 2")
-	require.Contains(t, client.comments[0].Body, "> [!TIP]")
+	client.AssertExpectations(t)
 }
 
 func TestFormat_CommentError(t *testing.T) {
@@ -115,121 +123,61 @@ func TestFormat_CommentError(t *testing.T) {
 	}
 
 	client := &fakeGitHubClient{}
+	client.On("FileComment", mock.Anything).Return(fmt.Errorf("comment error"))
+
 	formatter := New(io.Discard, client)
 	err := formatter.Format("test", i, result)
 
-	require.NoError(t, err)
-	require.Len(t, client.fileComments, 1)
+	require.Error(t, err)
+	require.Equal(t, "comment error", err.Error())
+
+	client.AssertExpectations(t)
 }
 
-func TestResolveFileComment(t *testing.T) {
-	client := &fakeGitHubClient{}
-	formatter := New(io.Discard, client)
-
-	comment := github.Comment{Body: "<!-- manifest:test:file:1:side -->", Type: github.FileComment, Stale: true}
-	client.comments = append(client.comments, comment)
-
+func TestFormat_Deduplicates(t *testing.T) {
 	i := &manifest.Import{
 		Pull: &manifest.Pull{
 			Number: 1,
 		},
 	}
 
-	err := formatter.BeforeAll(i)
-	require.NoError(t, err)
-
-	err = formatter.Format("test", i, manifest.Result{
+	result := manifest.Result{
 		Comments: []manifest.Comment{
-			{Text: "Another comment", Severity: manifest.SeverityError, File: "file", Line: 1, Side: "side"},
-		},
-	})
-
-	err = formatter.AfterAll(i)
-
-	assert.NoError(t, err)
-	require.Len(t, client.resolvedComments, 1)
-	require.Equal(t, comment.Body, client.resolvedComments[0].Body)
-}
-
-func TestResolveComment(t *testing.T) {
-	client := &fakeGitHubClient{}
-	formatter := New(io.Discard, client)
-
-	comment := github.Comment{Body: "<!-- manifest:test -->", Type: github.ReviewComment, Stale: true}
-	client.comments = append(client.comments, comment)
-
-	i := &manifest.Import{
-		Pull: &manifest.Pull{
-			Number: 1,
+			{
+				Text:     "Test comment",
+				Severity: manifest.SeverityError,
+			},
+			{
+				Text:     "File comment!",
+				Severity: manifest.SeverityError,
+				File:     "test.go",
+				Line:     10,
+				Side:     "RIGHT",
+			},
 		},
 	}
 
-	err := formatter.BeforeAll(i)
-	require.NoError(t, err)
-
-	err = formatter.Format("test", i, manifest.Result{
-		Comments: []manifest.Comment{
-			{Text: "Another comment", Severity: manifest.SeverityError},
-		},
-	})
-
-	err = formatter.AfterAll(i)
-
-	assert.NoError(t, err)
-	require.Len(t, client.resolvedComments, 1)
-	require.Equal(t, comment.Body, client.resolvedComments[0].Body)
-}
-
-func TestUnresolveComment(t *testing.T) {
 	client := &fakeGitHubClient{}
+
+	client.On("Comments", 1).Return([]github.Comment{
+		{Body: "<!-- manifest:test -->", Type: github.ReviewComment},
+		{Body: "<!-- manifest:test:test.go:10:RIGHT -->", Type: github.FileComment},
+	}, nil)
+	client.On("ReviewComments", 1).Return([]github.Comment{
+		{Body: "<!-- manifest:test:test.go:10:RIGHT -->", Type: github.FileComment},
+	}, nil)
+	client.On("UnresolveComment", mock.Anything).Return(nil)
+	client.On("UnresolveFileComment", mock.Anything).Return(nil)
+
 	formatter := New(io.Discard, client)
-
-	comment := github.Comment{Body: "~~<!-- manifest:test -->~~", Type: github.ReviewComment}
-	client.comments = append(client.comments, comment)
-
-	i := &manifest.Import{
-		Pull: &manifest.Pull{
-			Number: 1,
-		},
-	}
-
 	err := formatter.BeforeAll(i)
 	require.NoError(t, err)
-
-	err = formatter.Format("test", i, manifest.Result{
-		Comments: []manifest.Comment{
-			{Text: "Test comment", Severity: manifest.SeverityError},
-		},
-	})
-
-	assert.NoError(t, err)
-	require.Len(t, client.unresolvedComments, 1)
-	require.Equal(t, comment.Body, client.unresolvedComments[0].Body)
-}
-
-func TestUnresolveFileComment(t *testing.T) {
-	client := &fakeGitHubClient{}
-	formatter := New(io.Discard, client)
-
-	comment := github.Comment{Body: "~~<!-- manifest:test:file:1:side -->~~", Type: github.FileComment}
-	client.comments = append(client.comments, comment)
-
-	i := &manifest.Import{
-		Pull: &manifest.Pull{
-			Number: 1,
-		},
-	}
-
-	err := formatter.BeforeAll(i)
+	err = formatter.Format("test", i, result)
 	require.NoError(t, err)
 
-	err = formatter.Format("test", i, manifest.Result{
-		Comments: []manifest.Comment{
-			{Text: "Test comment", Severity: manifest.SeverityError, File: "file", Line: 1, Side: "side"},
-		},
-	})
+	client.AssertExpectations(t)
 
-	assert.NoError(t, err)
-	require.Len(t, client.unresolvedComments, 1)
-	require.Equal(t, comment, client.unresolvedComments[0])
+	client.AssertNotCalled(t, "FileComment", mock.Anything)
+	client.AssertCalled(t, "UnresolveComment", mock.Anything)
+	client.AssertCalled(t, "UnresolveFileComment", mock.Anything)
 }
